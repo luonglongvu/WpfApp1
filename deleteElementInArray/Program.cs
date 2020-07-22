26ff436af188da68dfe77a60dd247a68b36d1f52
vũ lương long <vuluonghaohoa@gmail.com>
2020-07-22 12:59:25 +0700

    Add project files.

---
+++ deleteElementInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deleteElementInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (isPrime(a[i]))
+                {
+                    Console.Write(a[i] + " ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+        public static bool isPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+    }
+}
